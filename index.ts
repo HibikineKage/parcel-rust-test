a328d9d21582b6c81f8a328235db5efd207599f8
Hibikine Kage <goodsmile314159@gmail.com>
2018-11-05 09:16:40 +0900

    add clog

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
-import { foo } from './Cargo.toml';
+import { foo, use_console_log, use_macro_console_log } from './Cargo.toml';
 console.log(foo('abc'));
+use_console_log();
+use_macro_console_log();
--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,27 @@ pub fn foo(x: &str) -> String {
         "no".to_string()
     }
 }
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: u32);
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_many(a: &str, b: &str);
+}
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+#[wasm_bindgen]
+pub fn use_macro_console_log() {
+    console_log!("Hello {}!", "world");
+    console_log!("Lets print some numbers");
+    console_log!("1 + 3 = {}", 1 + 3);
+}
+#[wasm_bindgen]
+pub fn use_console_log() {
+    log("Hello from rust!");
+    log_u32(42);
+    log_many("Hello world!", "Secondary log!");
+}
